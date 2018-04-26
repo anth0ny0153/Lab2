0604378868de2ff3da9efd669effe44a9cdaaa06
Anthony <anth0ny0153@gmail.com>
2018-04-26 14:05:07 -0400

    3rd commit changed calculations into methods and input into methods

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -17,24 +17,29 @@ namespace Lab2
                 double width = 0;
                 double area = 0;
                 double perimeter = 0;
-                String userInput;
-                //get user input 
-                
-                Console.WriteLine("Please enter the length of the room");
-                userInput = Console.ReadLine();
-                length = double.Parse(userInput);
-
-                Console.WriteLine("Please enter the width of the room");
-                userInput = Console.ReadLine();
-                width = double.Parse(userInput);
+                        //String userInput;
+                        //get user input 
+                length = ReadDoubles("Please enter the length of the room");
+                        //Console.WriteLine("Please enter the length of the room");
+                        //userInput = Console.ReadLine();
+                        //length = double.Parse(userInput);
+                width = ReadDoubles("Please enter the width of the room");
+                        //Console.WriteLine("Please enter the width of the room");
+                        //userInput = Console.ReadLine();
+                        //width = double.Parse(userInput);
 
                 //calculations
-                area = length * width;
-                perimeter = 2 * (length + width);
+                area = CalcArea(length, width);
+                        //area = length * width;
+                perimeter = CalcPerimeter(length, width);
+                        //perimeter = 2 * (length + width);
+
                 Console.WriteLine("The area of the room is: " + area);
+
                 Console.WriteLine("The perimeter of the room is: " + perimeter);
-                //Console.WriteLine($"The perimeter of the room is: {perimeter} ");
-                              //a different way to do it ^
+                        //Console.WriteLine($"The perimeter of the room is: {perimeter} ");
+                        //a different way to do it ^
+
 
                 //rerun program
                 Console.WriteLine("Do you want to run the program again? (yes/no)");
@@ -42,6 +47,26 @@ namespace Lab2
                     break;
                 
             }
+        }
+        public static double ReadDoubles(string message)
+        {
+            Console.WriteLine(message);
+            double input = double.Parse(Console.ReadLine());
+            return input;
+        }
+        public static double CalcArea(double length, double width)
+        {
+            double area;
+            area = length * width;
+
+            return area;
+        }
+        public static double CalcPerimeter(double length, double width)
+        {
+            double perimeter;
+            perimeter = 2 * (length + width);
+
+            return perimeter;
         }
     }
 }
